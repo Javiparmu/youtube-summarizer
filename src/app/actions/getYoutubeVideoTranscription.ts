41a3208c8f12c9ccc1ec9309d4b6d4d8cd41d3c4
javiparmu <mrjavistinson@gmail.com>
2024-10-10 00:06:55 +0200

    update cookies and rotate ip

--- src/app/actions/getYoutubeVideoTranscription.ts
+++ src/app/actions/getYoutubeVideoTranscription.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import ytdl from '@distube/ytdl-core'
+import randomIpv6 from 'random-ipv6'
 
 const youtubeCookies = [
   { name: 'YSC', value: 'VW73Ou8J1Lo' },
@@ -19,23 +20,23 @@ const youtubeCookies = [
   { name: 'SID', value: 'g.a000oghPhGNb08CU-K32z7SPzMH2NpT-lSqG5yCekvt-hIwPba6Wqvh-9EG7ADeYQzEK9L0rIAACgYKAaISARISFQHGX2MiO-zfY9DcAUYiiDWMItZAGxoVAUF8yKpVaRN0MY4JQU8bmaDyJ2Lp0076' },
   { name: '__Secure-1PSID', value: 'g.a000oghPhGNb08CU-K32z7SPzMH2NpT-lSqG5yCekvt-hIwPba6WlhAOel9JiYqsklhl0Nc2bQACgYKAdgSARISFQHGX2Mi1E-bfZZyuLGTjfD13wBg5BoVAUF8yKqOBekCOhSroNFRPHfRfDOz0076' },
   { name: '__Secure-3PSID', value: 'g.a000oghPhGNb08CU-K32z7SPzMH2NpT-lSqG5yCekvt-hIwPba6WQvUeAwSm8uTlRUR4NySaZgACgYKAR4SARISFQHGX2MiYboLncGX-3FdFnnGF6R-IRoVAUF8yKoredkSrzGX0xNs6IWGS3r20076' },
-  { name: '__Secure-1PSIDTS', value: 'sidts-CjEBQlrA-D8Jyv0lBQI3ALHOXjEF4NEgvAe0znKdUG39aFx8kgfphoJWeuVnf9Ez1NxEEAA' },
-  { name: '__Secure-3PSIDTS', value: 'sidts-CjEBQlrA-D8Jyv0lBQI3ALHOXjEF4NEgvAe0znKdUG39aFx8kgfphoJWeuVnf9Ez1NxEEAA' },
-  { name: 'ST-l3hjtt', value: 'session_logininfo=AFmmF2swRgIhAOkU2D6AH92URGoFqQ1urut_KR0xMAvJOr1TdL4RGBKGAiEA5WUb9erCekxvHxx-vqp939TPGjAXJG-kx-O1Wa5ir9s%3AQUQ3MjNmelhzWThfUThreU5PUGNneWlnQk5oazNyTW1KUEdfbElCOEZiUTQyY05CQ1JjS01HYlZMbUp6akFYN3ZJUG0tbjZiV2w3OHdJTFlHdHZLZXlOenozZnF2eWwtQUF5RTZDaG5kM2h6bUhRSEFfbVlrZFUyWlg1V21NaUJ2RmlBbHc2M2RSV2dVMDZlS3FLeTRwMFJ2Z0FhREM1bHJwV2pkajZadExxX3BUX3ZzNi1hdHFrdFEzZHNLWE41N3dNVXg5cVRKaEVvQ2hHLXZYWDlqcTQ1dTlJbWp6Wnlydw%3D%3D' },
-  { name: 'ST-3opvp5', value: 'session_logininfo=AFmmF2swRgIhAOkU2D6AH92URGoFqQ1urut_KR0xMAvJOr1TdL4RGBKGAiEA5WUb9erCekxvHxx-vqp939TPGjAXJG-kx-O1Wa5ir9s%3AQUQ3MjNmelhzWThfUThreU5PUGNneWlnQk5oazNyTW1KUEdfbElCOEZiUTQyY05CQ1JjS01HYlZMbUp6akFYN3ZJUG0tbjZiV2w3OHdJTFlHdHZLZXlOenozZnF2eWwtQUF5RTZDaG5kM2h6bUhRSEFfbVlrZFUyWlg1V21NaUJ2RmlBbHc2M2RSV2dVMDZlS3FLeTRwMFJ2Z0FhREM1bHJwV2pkajZadExxX3BUX3ZzNi1hdHFrdFEzZHNLWE41N3dNVXg5cVRKaEVvQ2hHLXZYWDlqcTQ1dTlJbWp6Wnlydw%3D%3D' },
-  { name: 'ST-xuwub9', value: 'session_logininfo=AFmmF2swRgIhAOkU2D6AH92URGoFqQ1urut_KR0xMAvJOr1TdL4RGBKGAiEA5WUb9erCekxvHxx-vqp939TPGjAXJG-kx-O1Wa5ir9s%3AQUQ3MjNmelhzWThfUThreU5PUGNneWlnQk5oazNyTW1KUEdfbElCOEZiUTQyY05CQ1JjS01HYlZMbUp6akFYN3ZJUG0tbjZiV2w3OHdJTFlHdHZLZXlOenozZnF2eWwtQUF5RTZDaG5kM2h6bUhRSEFfbVlrZFUyWlg1V21NaUJ2RmlBbHc2M2RSV2dVMDZlS3FLeTRwMFJ2Z0FhREM1bHJwV2pkajZadExxX3BUX3ZzNi1hdHFrdFEzZHNLWE41N3dNVXg5cVRKaEVvQ2hHLXZYWDlqcTQ1dTlJbWp6Wnlydw%3D%3D' },
-  { name: 'SIDCC', value: 'AKEyXzVIySuKIq5wLL7LZEQk6Wp1mIUAPnvD7gQiWJ5WwTxjMme1Nftbpk12cafbN5zk8lyV5KJ5' },
-  { name: '__Secure-1PSIDCC', value: 'AKEyXzVEKLsESolKpxVuHa2jSNbZIcnrtFVkIzzD_UNJSdRSXwq_mXU4xqqn_uSCuXvXEwMX6Hk' },
-  { name: '__Secure-3PSIDCC', value: 'AKEyXzUO6VpcQjAL3CdbawDj79He-Rqo5nX7GLlqK_wOy7o3MvmsJ0dKcjZfjegtfIgZJg40FbSa' }
+  { name: '__Secure-1PSIDTS', value: 'sidts-CjEBQlrA-MBla61ElKUEXiJioEV3I6bj8bpGGtUa_pbIJeIH1Ar7ug6SRSkp2bKWZKNGEAA' },
+  { name: '__Secure-3PSIDTS', value: 'sidts-CjEBQlrA-MBla61ElKUEXiJioEV3I6bj8bpGGtUa_pbIJeIH1Ar7ug6SRSkp2bKWZKNGEAA' },
+  { name: 'SIDCC', value: 'AKEyXzX63Rl2GG5frAjV4dPsCE48uYKUtfp25MLRZY7k7d3zfCQ_DbUzXnYvjLa8sSsv6JQmaKUy' },
+  { name: '__Secure-1PSIDCC', value: 'AKEyXzVFbdSnN0tf5XEtrbY_5UMT1gcIi40Wc1Tw_KLKsAtaLNgkO6J1QUfjG64pOJ5eQwiHZ1U' },
+  { name: '__Secure-3PSIDCC', value: 'AKEyXzVk-y89YyZsoevtQ_f7ugoeZmt8TVD60g9F_IsC6dMM5KpXYTuGS_fIBOQSkBmcAUX_GgoX' }
 ];
-
 
 export const getYoutubeVideoTranscription = async (url: string) => {
   let videoInfo = null
   const videoId = ytdl.getVideoID(url)
 
   try {
-    const agent = ytdl.createAgent(youtubeCookies)
+    const agent = ytdl.createAgent(youtubeCookies, {
+      pipelining: 5,
+      maxRedirections: 0,
+      localAddress: randomIpv6()
+    })
     videoInfo = await ytdl.getBasicInfo(url, {
       agent
     })
